82584d7b7143ff4c99cc991cb5396dc5de4f868f
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-01-22 16:53:21 +0530

    Time: 0 ms (100%), Space: 43 MB (23.22%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long nn=n;
+        if(nn<0) nn=(-1)*nn;
+        double ans=1.0;
+        while(nn>0){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
+            }
+            else{
+                ans=ans*x;
+                nn=nn-1;
+            }
+        }
+        if(n<0) ans= 1.0/ans;
+        return ans;
+    }
+}
