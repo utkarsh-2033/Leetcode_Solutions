6dfdafbe85261efa3857b7985c435931e4e58469
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-02-11 19:00:39 +0530

    Time: 0 ms (100%), Space: 41.6 MB (84.98%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,28 +14,15 @@
  * }
  */
 class Solution {
-    public TreeNode construct(int preorder[], int inorder[],int preidx[], 
-    Map<Integer,Integer> mp ,int left,int right ){
-        if(left>right) return null;
-        int rootval=preorder[preidx[0]];
-        TreeNode root=new TreeNode(rootval);
-        preidx[0]++;
-        int inorderidx=mp.get(rootval);
-        root.left=construct(preorder,inorder,preidx,mp,left,inorderidx-1);
-        root.right=construct(preorder,inorder,preidx,mp,inorderidx+1,right);
+    public TreeNode construct(int[] pre , int idx[] , int ub){
+        if(idx[0]>=pre.length || pre[idx[0]]>ub) return null;
+        TreeNode root= new TreeNode(pre[idx[0]++]);
+        root.left=construct(pre,idx,root.val); //left upp bound= root.val
+        root.right=construct(pre,idx,ub);      // right ub= bound of root
         return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
-        //using property that inorder of bst is sorted
-        // then using general concept of creating tree from preorder and inorder
-        int []inorder=preorder.clone();
-        Arrays.sort(inorder);
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<inorder.length;i++){
-            mp.put(inorder[i],i);
-        }
-        int preidx[]={0};
-        TreeNode root=construct(preorder,inorder,preidx,mp,0,inorder.length-1);
-        return root;
+        int idx[]={0};
+        return construct(preorder,idx,Integer.MAX_VALUE);
     }
 }
