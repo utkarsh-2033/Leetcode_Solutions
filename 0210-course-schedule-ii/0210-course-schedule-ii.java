0ffa5152e8938d663f89cd11989465208651313d
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-02-15 17:54:48 +0530

    Time: 5 ms (77.9%), Space: 46.1 MB (38.7%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        Queue<Integer> q =new LinkedList<>();
+        ArrayList<Integer> topo=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        int indeg[]=new int[numCourses];
+        //creating graph -adjacency matrix
+        for(int it[]:prerequisites){
+            int v=it[0];
+            int u=it[1];
+            adj.get(u).add(v); // directed
+            indeg[v]++; //creating indegree array
+        }
+        //inserting vertices with 0 indeg
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.offer(i);
+        }
+        while(!q.isEmpty()){
+            int node=q.poll();
+            topo.add(node);
+            for(int adjele:adj.get(node)){
+                indeg[adjele]--;
+                if(indeg[adjele]==0) q.offer(adjele);
+            }
+        }
+        if(topo.size()==numCourses){
+            int ans[]=new int[topo.size()]; int j=0;
+            for(int i:topo) ans[j++]=i;
+            return ans;
+        }
+        return new int[0];
+    }
+}
