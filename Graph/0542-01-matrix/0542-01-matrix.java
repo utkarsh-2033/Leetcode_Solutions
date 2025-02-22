4c4e2004684d2af9c6b9fee8d97ef222a2685d18
utkarsh-2033 <utkarshg@2033>
2025-02-22 20:11:52 +0530

    solved  graph algorithms: smallest divisor, bipartite check, word ladder, course scheduling, and path with minimum effort; remove unused split array and matrix update implementations.

--- Graph/0542-01-matrix/0542-01-matrix.java
+++ Graph/0542-01-matrix/0542-01-matrix.java
@@ -3,32 +3,28 @@ class Solution {
         int m=mat.length;
         int n=mat[0].length;
         int visited[][]=new int[m][n];
-        Queue<int[]> q=new LinkedList<>(); //int[]= {row,col,distance}
-        //inserting the initial Os 
+        Queue<int[]> q=new LinkedList<>(); //{r,c,steps}
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){ 
-                    q.offer(new int[]{i,j,0});// distace of 0 from 0=0
+                    q.offer(new int[]{i,j,0});
                     visited[i][j]=1;
-                    }
+                }
             }
         }
         int delrow[]={-1,0,1,0};
         int delcol[]={0,-1,0,1};
         while(!q.isEmpty()){
-            int r=q.peek()[0];
-            int c=q.peek()[1];
-            int dist=q.peek()[2];
-            q.poll();
-            mat[r][c]=dist;      // marking the distance in the same input matrix
-            //traversing the 4 dircns
+            int[] k=q.poll();
+            int r=k[0];
+            int c=k[1];
+            int steps=k[2];
+            mat[r][c]=steps;
             for(int i=0;i<4;i++){
                 int nr=r+delrow[i];
                 int nc=c+delcol[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n 
-                && mat[nr][nc]==1 
-                && visited[nr][nc]==0){
-                    q.offer(new int[]{nr,nc,dist+1});
+                if(nr>=0 && nc>=0 && nr<m && nc<n && visited[nr][nc]==0){
+                    q.offer(new int[]{nr,nc,steps+1});
                     visited[nr][nc]=1;
                 }
             }
