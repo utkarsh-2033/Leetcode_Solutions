477567aa94dc522a86c013a19cb66262078382a4
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-03-18 18:38:12 +0530

    Time: 1 ms (99.42%), Space: 45.2 MB (50.27%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0; //count of jumps
+        int maxReach=0; // track of farthest posn that can be reached from that index
+        int end=0; //tracking jump boundary (when we must jump).
+        for(int i=0;i<nums.length-1;i++){ //n-1 : no need to make jump from the last idx , since already reached last idx(destination)
+            maxReach=Math.max(maxReach,i+nums[i]);
+            if(i==end){ //jumping only when forced to(reached end of current boundary)
+                jumps=jumps+1; 
+                end=maxReach; //setting next boundary to the current 
+                              //idx maxReach(farthest that can be reached from that index)
+                if (end >= nums.length - 1) break ;//  we can reach last index already, stop
+            }
+        }
+        return jumps;
+    }
+}
