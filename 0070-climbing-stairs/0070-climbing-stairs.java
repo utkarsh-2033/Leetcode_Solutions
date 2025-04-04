0b36512c3c900a29b7ad42f601e9722ef648c51e
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-04 23:15:16 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
-    int recurr(int idx,int []dp){
-        if(idx==0) return 1; //base case
-        if(dp[idx]!=-1) return dp[idx];
-        int fs= recurr(idx-1,dp);
-        int ss=0;
-        if(idx>1)
-        ss= recurr(idx-2,dp);
-        return dp[idx]=fs+ss;
+    int recurr(int idx, int[] dp) {
+        if (idx == 0)
+            return 1; //base case
+        if (dp[idx] != -1)
+            return dp[idx];
+        int fs = recurr(idx - 1, dp);
+        int ss = 0;
+        if (idx > 1)
+            ss = recurr(idx - 2, dp);
+        return dp[idx] = fs + ss;
     }
+
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        for(int i=0;i<n+1;i++) dp[i]=-1;
-        return recurr(n,dp);
+        //tabulation-bottom to top
+        int dp[] = new int[n + 1];
+        // for(int i=0;i<n+1;i++) dp[i]=-1;
+        dp[0] = 1;
+        for (int i = 1; i < n + 1; i++) {
+            int fs = dp[i-1];
+            int ss = 0;
+            if (i > 1)
+                ss = dp[i-2];
+            dp[i] = fs + ss;
+        }
+        return dp[n];
     }
 }
