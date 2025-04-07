f9a63a38c406c3fd1d12715699065a66ea03ebc1
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-08 01:07:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (50.67%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     int paths(int i,int j,int grid[][],int dp[][]){
-        if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(grid[i][j]==1) return 0;
+        if(i==0 && j==0) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
         int up=paths(i-1,j,grid,dp);
         int left=paths(i,j-1,grid,dp);
