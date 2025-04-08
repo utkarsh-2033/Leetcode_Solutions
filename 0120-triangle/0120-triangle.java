91571ab4ab21a941c8126033fce664184309a938
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-08 23:49:50 +0530

    Time: 1 ms (99.81%), Space: 44.1 MB (67.33%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,23 @@
+class Solution {
+    // grid - fixed to varible
+    // starting from fixed point
+    int pathsum(int i,int j,List<List<Integer>> triangle ,int n,int dp[][]){
+        if(j<0 || j>i) return (int)1e8; //out of boundary condns
+        if(i==n-1){  // base case - destination - last row
+            return triangle.get(n-1).get(j);
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int min=Integer.MAX_VALUE;
+        //moving down and diagonally-down -> adding cell value and then exploeing further paths
+        int down=triangle.get(i).get(j)+ pathsum(i+1,j,triangle,n,dp);
+        int dia=triangle.get(i).get(j)+ pathsum(i+1,j+1,triangle,n,dp);
+        min=Math.min(down,dia);
+        return dp[i][j]=min;  
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        int dp[][]=new int[n][n];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return pathsum(0,0,triangle,n,dp);
+    }
+}
