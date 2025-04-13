d0736760058469d67bfa0075a6c51147695db1b4
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-13 23:47:00 +0530

    Time: 97 ms (15.85%), Space: 75.3 MB (5.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,22 @@
-public class Solution {
-    public boolean partition(int[] nums, int idx, int target) {
-        if (target == 0) return true;  // base case-  subset found
-        if (idx < 0 || target < 0) return false;
-
-        // Include or exclude the current number
-        return partition(nums, idx - 1, target - nums[idx]) ||
-               partition(nums, idx - 1, target);
+class Solution {
+    private boolean f(int idx,int target,int arr[],int dp[][]){
+        if(target==0) return true;
+        if(idx==0) return arr[0]==target;
+        if(dp[idx][target]!=-1) return (dp[idx][target]==0)? false : true;
+        boolean nottake=f(idx-1,target,arr,dp);
+        boolean take=false;
+        if(arr[idx]<=target) take=f(idx-1,target-arr[idx],arr,dp);
+        dp[idx][target]=(take||nottake)?1:0;
+        return take||nottake;
     }
 
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % 2 != 0) return false;
-        int target = sum / 2;
-        return partition(nums, nums.length - 1, target);
+        int n=nums.length;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        if(sum%2!=0) return false; // if sum is odd , can not be divided into 2 eqaul sum
+        int dp[][]=new int[n][sum+1];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return f(n-1,sum/2,nums,dp);
     }
 }
