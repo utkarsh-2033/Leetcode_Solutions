d541f6a56bb432703e1eb18f58e82a8778e6cf85
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-14 23:47:30 +0530

    Time: 48 ms (38.23%), Space: 45.1 MB (53.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,39 @@
-public class Solution {
-    public boolean partition(int[] nums, int idx, int target,Boolean dp[][]) {
-        if (target == 0) return true;  // base case-  subset found
-        if (idx < 0 || target < 0) return false;
-        if (dp[idx][target] != null) return dp[idx][target];
-
-        boolean include = partition(nums, idx - 1, target - nums[idx], dp);
-        boolean exclude = partition(nums, idx - 1, target, dp);
-
-        return dp[idx][target] = include || exclude;
+class Solution {
+    // private boolean f(int idx,int target,int arr[],int dp[][]){
+    //     if(target==0) return true;
+    //     if(idx==0) return arr[0]==target;
+    //     if(dp[idx][target]!=-1) return (dp[idx][target]==0)? false : true;
+    //     boolean nottake=f(idx-1,target,arr,dp);
+    //     boolean take=false;
+    //     if(arr[idx]<=target) take=f(idx-1,target-arr[idx],arr,dp);
+    //     dp[idx][target]=(take||nottake)?1:0;
+    //     return take||nottake;
+    // }
+    static Boolean isSubsetSum(int arr[], int sum) {
+        int n=arr.length;
+        boolean prev[]=new boolean[sum+1];
+        prev[0]=true; //first col
+        if(arr[0]<=sum) prev[arr[0]]=true;   //1st row 
+        for(int idx=1;idx<n;idx++){
+            boolean curr[]=new boolean[sum+1];
+            curr[0]=true;
+            for(int target=1;target<=sum;target++){
+                boolean nottake=prev[target];
+                boolean take=false;
+                if(arr[idx]<=target) take=prev[target-arr[idx]];
+                curr[target]=take||nottake;
+            }
+            prev=curr;
+        }
+        return prev[sum];
     }
 
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % 2 != 0) return false;
-        int target = sum / 2;
-        int n = nums.length;
-        Boolean[][] dp = new Boolean[n][target + 1];
-        return partition(nums, n - 1, target, dp);
+        int n=nums.length;
+        int sum=0;
+        for(int i:nums) sum+=i;
+        if(sum%2!=0) return false; // if sum is odd , can not be divided into 2 eqaul sum
+        
+        return isSubsetSum(nums,sum/2);
     }
 }
