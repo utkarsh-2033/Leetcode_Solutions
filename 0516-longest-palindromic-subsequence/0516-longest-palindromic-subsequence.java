c18ef1d85603d2f5f6e84ab16ad5937b04644d7e
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-19 00:32:59 +0530

    Time: 54 ms (45.08%), Space: 45.5 MB (91.68%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -3,15 +3,15 @@ class Solution {
         String rev=new StringBuilder(s).reverse().toString();
         int n=s.length();
         int prev[]=new int[n+1];
-        for(int i=1;i<n+1;i++){
         int curr[]=new int[n+1];
+        for(int i=1;i<n+1;i++){
             for(int j=1;j<=n;j++){
                 if(s.charAt(i-1)==rev.charAt(j-1)) 
                 curr[j]=1+prev[j-1];
                 else 
                 curr[j]=Math.max(prev[j],curr[j-1]);
             }
-            prev=curr;
+            prev=curr.clone();
         }
         return prev[n];
     }
