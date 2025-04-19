9a7343f824c8ce31ac358fccf6dd68301a0e57f8
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-19 22:57:18 +0530

    Time: 17 ms (86.08%), Space: 51.2 MB (68.3%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,21 +1,19 @@
 class Solution {
-    private int f(int i,int j,String s ,String t,int dp[][]){
-        if(j==0) return 1; // t gets exhausted => 1 subseq matched found
-        if(i==0) return 0; // s gets exhausted => no subseq matched found in this path
-        if(dp[i][j]!=-1) return dp[i][j];
-        // characters matched
-        if(s.charAt(i-1)==t.charAt(j-1))  
-        // move to to next idx on both to check next chracaters matching 
-        //or remain on same t idx and next idx in s to check for other possibilities of same character present in s 
-        return dp[i][j]=f(i-1,j-1,s,t,dp) + f(i-1,j,s,t,dp);
-        //else - character didnt match - move to next chracter in s to match
-        return dp[i][j]=f(i-1,j,s,t,dp);
-    }
+   
     public int numDistinct(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        int dp[][]=new int[n+1][m+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return f(n,m,s,t,dp);
+        int n = s.length();
+        int m = t.length();
+        int dp[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = 1; //base case (j==0)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1]  + dp[i - 1 ][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 }
