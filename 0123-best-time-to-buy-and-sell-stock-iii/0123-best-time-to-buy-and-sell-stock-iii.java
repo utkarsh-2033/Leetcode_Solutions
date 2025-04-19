b38b83642c5887d9d0991f93c4c58bc2dfadedab
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-04-20 01:59:35 +0530

    Time: 57 ms (43.91%), Space: 63.2 MB (39.75%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,19 @@
 class Solution {
-    //taking 3rd parameter - like knapsack - capacity - two ensure max 2 traansaction 
-    // 1 transaction = buy + sell so cap-- only when sell
-    //addition base case - when transactions(cap) exhaust - return
-    private int f(int idx,int buy,int cap,int prices[],int dp[][][]){
-        if(idx==prices.length) return 0;
-        if(cap==0) return 0;
-        if(dp[idx][buy][cap]!=-1) return dp[idx][buy][cap];
-        if(buy==1){
-            return dp[idx][buy][cap]= Math.max(-prices[idx]+ f(idx+1,0,cap,prices,dp),f(idx+1,1,cap,prices,dp));
-        }
-        else{
-            return dp[idx][buy][cap]= Math.max(prices[idx]+ f(idx+1,1,cap-1,prices,dp),f(idx+1,0,cap,prices,dp));
-        }
-    }
+    //tabulation
     public int maxProfit(int[] prices) {
-        int n=prices.length;
-        int dp[][][]=new int[n][2][3];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++){
-                for(int k=0;k<3;k++) dp[i][j][k]=-1;
+        int n = prices.length;
+        int dp[][][] = new int[n + 1][2][3];
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int cap = 1; cap < 3; cap++) {
+                    if (buy == 1) {
+                        dp[idx][buy][cap] = Math.max(-prices[idx] + dp[idx+1][0][cap], dp[idx+1][1][cap]);
+                    } else {
+                        dp[idx][buy][cap] = Math.max(prices[idx] + dp[idx+1][1][cap-1], dp[idx+1][0][cap]);
+                    }
+                }
             }
         }
-        return f(0,1,2,prices,dp);
+        return dp[0][1][2];
     }
 }
