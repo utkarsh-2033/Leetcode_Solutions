113a8af49a0a27b1d81665cdb2a2c2e797d39f59
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-05-24 19:20:14 +0530

    Time: 3 ms (87.97%), Space: 95.5 MB (49.61%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int[] allowed = new int[n + 1];  // one extra for easier prefix handling
-        for (int[] q : queries) {
-            int l = q[0];
-            int r = q[1];
-            allowed[l] += 1;
-            if (r + 1 < n) {
-                allowed[r + 1] -= 1;
-            }
+        int n=nums.length;
+        int diff[]=new int[n+1];
+        //applying diff array techniq to count max operation 
+        //that can be performed at any idx i in nums
+        for(int q[]:queries){
+            int l=q[0];
+            int r=q[1];
+            diff[l]+=1;
+            diff[r+1]-=1;
         }
-        for (int i = 1; i < n; i++) {
-            allowed[i] += allowed[i - 1];
+        //finding ps 
+        for(int i=1;i<n+1;i++){
+            diff[i]+=diff[i-1];
         }
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > allowed[i]) return false;
+        //checking if total op that can be performed at any idx i less than the nums[i]- return false
+        //each opertion - can decrement value by 1
+        for(int i=0;i<n;i++){
+            if(nums[i]>diff[i]) return false;
         }
         return true;
     }
