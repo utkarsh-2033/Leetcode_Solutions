3c61b6290617997a82d5e9cc78a443414fcca7c9
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2025-07-08 00:29:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -1,15 +1,18 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        HashSet<Integer> hs=new HashSet<>();
-        Arrays.sort(events,(a,b)->(a[1]!=b[1])?a[1]-b[1]:a[0]-b[0]);
+        //sorting accn to end time
+        //attending event which gets completed first greedily to maximize no of events attended
+        Arrays.sort(events,(a,b)->(a[1]-b[1]));
+        int maxendtime=-1;
+        for(int i=0;i<events.length;i++) maxendtime=Math.max(maxendtime,events[i][1]);
+        int hash[]=new int[maxendtime+2];
         int c=0;
         for(int i=0;i<events.length;i++){
-            int start=events[i][0];
-            int end=events[i][1];
-            for(int j=end;j>=start;j--){
-                if(!hs.contains(j)){
+            // int j=events[i][0];
+            for(int j=events[i][0];j<=events[i][1];j++){
+                if(hash[j]==0){
+                    hash[j]=i+1;
                     c++;
-                    hs.add(j);
                     break;
                 }
             }
